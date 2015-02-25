d097dc46eeda81128af421b1aec7f6ca4bd2ec95
ILoveSpeccy <ilovespeccy@speccyland.net>
2015-02-25 20:53:23 +0100

    Added "Test" FPGA core

---
+++ cores/test/src/test.vhd
@@ -0,0 +1,32 @@
+library std;
+use std.textio.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity test is
+   port (
+      CLK: in std_logic;
+      LEDG : out std_logic_vector(7 downto 0));
+end test;
+
+architecture rtl of test is
+
+    signal TICK : unsigned(25 downto 0);
+	 
+begin
+	  
+process(CLK)
+begin
+   if rising_edge(CLK) then
+      TICK <= TICK + 1;
+   end if;
+end process;
+
+LEDG <= std_logic_vector(TICK(25 downto 18));
+
+end rtl;
+
+
+
